532a7fdc01e030fed96b82c9c654e46f076a66ac
weijie.huang <weijie.huang@brilliant-solution.com>
2022-04-21 10:52:45 +0800

    Initial commit

---
+++ src/main/java/com/hwj/jenkinstest/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.hwj.jenkinstest.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/test")
+@RestController
+public class TestController {
+
+    @GetMapping("/getString")
+    public String getString(){
+        return "test";
+    }
+}
